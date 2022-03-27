1f30f7477877cdae484443f9f027d249820847ee
Konst_V <varjutin@yandex.ru>
2022-03-27 13:05:19 +0300

    форматирвование текста

--- js/script.js
+++ js/script.js
@@ -31,9 +31,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
   });
 
-  function mobileSliderBrands () {
+  function mobileSliderBrands() {
 
-    if(window.innerWidth <= 767 && sliderBrands.dataset.mobile === "false") {
+    if (window.innerWidth <= 767 && sliderBrands.dataset.mobile === "false") {
       swiperBrands = new Swiper(sliderBrands, {
         speed: 600,
         spaceBetween: 16,
@@ -53,21 +53,21 @@ document.addEventListener("DOMContentLoaded", () => {
         },
       });
 
-      sliderBrands.dataset.mobile ="true";
+      sliderBrands.dataset.mobile = "true";
     }
 
-    if(window.innerWidth > 767){
-      sliderBrands.dataset.mobile ="false";
-      if(sliderBrands.classList.contains('swiper-initialized')) {
+    if (window.innerWidth > 767) {
+      sliderBrands.dataset.mobile = "false";
+      if (sliderBrands.classList.contains('swiper-initialized')) {
         swiperBrands.destroy();
       }
     }
   }
 
 
-  function mobileSliderTypes () {
+  function mobileSliderTypes() {
 
-    if(window.innerWidth <= 767 && sliderTypes.dataset.mobile === "false") {
+    if (window.innerWidth <= 767 && sliderTypes.dataset.mobile === "false") {
       swiperTypes = new Swiper(sliderTypes, {
         // Optional parameters
         speed: 600,
@@ -92,9 +92,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     }
 
-    if(window.innerWidth > 767){
-      sliderTypes.dataset.mobile ="false";
-      if(sliderTypes.classList.contains('swiper-initialized')) {
+    if (window.innerWidth > 767) {
+      sliderTypes.dataset.mobile = "false";
+      if (sliderTypes.classList.contains('swiper-initialized')) {
         swiperTypes.destroy();
       }
     }
@@ -120,36 +120,32 @@ document.addEventListener("DOMContentLoaded", () => {
     if (heroInfo.clientHeight < heroInfo.scrollHeight) {
       heroInfo.style.maxHeight = document.querySelector('.hero-info').scrollHeight + "px"
       this.innerHTML = '<img class="btn-more__icon" src="./assets/images/expand-up.svg" alt="Двойная стрелка вверх">Скрыть'
-      if(window.innerWidth > 767) {
+      if (window.innerWidth > 767) {
         setTimeout(() => {
           heroInfo.style.overflow = "visible"
         }, 400)
-          }
-        else{
-          setTimeout(() => {
-            heroInfo.style.overflow = "visible"
-          },500)
-        }
+      } else {
+        setTimeout(() => {
+          heroInfo.style.overflow = "visible"
+        }, 500)
       }
-    else {
+    } else {
       this.innerHTML = '<img class="btn-more__icon" src="./assets/images/expand.svg" alt="Двойная стрелка вниз">Читать далее'
-      if(window.innerWidth >= 1120) {
+      if (window.innerWidth >= 1120) {
         heroInfo.style.maxHeight = "165px";
         setTimeout(() => {
           heroInfo.style.overflow = "hidden"
-        },100)
-      }
-      else if(window.innerWidth < 768){
+        }, 100)
+      } else if (window.innerWidth < 768) {
         heroInfo.style.maxHeight = "90px";
         setTimeout(() => {
           heroInfo.style.overflow = "hidden"
-        },1)
-      }
-      else{
+        }, 1)
+      } else {
         heroInfo.style.maxHeight = "160px";
         setTimeout(() => {
           heroInfo.style.overflow = "hidden"
-        },250)
+        }, 250)
       }
 
     }
@@ -157,23 +153,23 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
   let i;
-  for (i = 1; i <  btnMoreList.length; i++) {
-      btnMoreList[i].addEventListener("click", function() {
-        let panel = this.previousElementSibling;
-        console.log(panel.clientHeight);
-        console.log(panel.scrollHeight);
-        if (panel.clientHeight < panel.scrollHeight){
-          panel.style.maxHeight = panel.scrollHeight + "px";
-          this.innerHTML = '<img class="btn-more__icon" src="./assets/images/expand-up.svg" alt="Двойная стрелка вверх">Скрыть'
-        } else {
-          panel.style.maxHeight = "160px";
-          this.innerHTML = '<img class="btn-more__icon" src="./assets/images/expand.svg" alt="Двойная стрелка вниз">Показать все'
-        }
-      });
+  for (i = 1; i < btnMoreList.length; i++) {
+    btnMoreList[i].addEventListener("click", function() {
+      let panel = this.previousElementSibling;
+      console.log(panel.clientHeight);
+      console.log(panel.scrollHeight);
+      if (panel.clientHeight < panel.scrollHeight) {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+        this.innerHTML = '<img class="btn-more__icon" src="./assets/images/expand-up.svg" alt="Двойная стрелка вверх">Скрыть'
+      } else {
+        panel.style.maxHeight = "160px";
+        this.innerHTML = '<img class="btn-more__icon" src="./assets/images/expand.svg" alt="Двойная стрелка вниз">Показать все'
+      }
+    });
 
     // }
   }
-  
+
   const btnBurger = document.querySelector('.burger');
   const mobileMenu = document.querySelector('.mobile-menu');
   const btnMenuClose = document.querySelector('.btn-close');
@@ -181,7 +177,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const btnChat = document.querySelectorAll('.btn-chat');
   const feedbackSection = document.querySelector('.modal--feedback');
   const feedbackCloseBtn = document.querySelector('.btn-close--feedback');
-  const backgroundOpacity  =  document.querySelector('.background-opacity');
+  const backgroundOpacity = document.querySelector('.background-opacity');
   const connectCloseBtn = document.querySelector('.btn-close--connect');
   const connectSection = document.querySelector('.modal--connect');
 
@@ -197,40 +193,31 @@ document.addEventListener("DOMContentLoaded", () => {
   btnBurger.addEventListener('click', () => displayBlock(mobileMenu, 'visin'));
   btnMenuClose.addEventListener('click', () => displayHidden(mobileMenu, 'visin'));
 
-  for(let btn of btnCall) {
+  for (let btn of btnCall) {
     btn.addEventListener('click', () => openSection(connectSection));
   }
 
   connectCloseBtn.addEventListener('click', () => closeSection(connectSection));
 
-  for(let btn of btnChat) {
+  for (let btn of btnChat) {
     btn.addEventListener('click', () => openSection(feedbackSection));
   }
 
   feedbackCloseBtn.addEventListener('click', () => closeSection(feedbackSection));
 
   function closeSection(elem) {
-    displayHidden(backgroundOpacity,'block-opacity');
-    displayHidden(elem,'modal-transform');
-    displayBlock(elem,'hidden');
+    displayHidden(backgroundOpacity, 'block-opacity');
+    displayHidden(elem, 'modal-transform');
+    displayBlock(elem, 'hidden');
   }
-  function openSection(elem ) {
-    displayBlock(backgroundOpacity,'block-opacity');
+
+  function openSection(elem) {
+    displayBlock(backgroundOpacity, 'block-opacity');
     displayHidden(mobileMenu, 'visin');
     setTimeout(() => {
-      displayHidden(elem,'hidden')
-      displayBlock(elem,'modal-transform')
-    },500)
+      displayHidden(elem, 'hidden')
+      displayBlock(elem, 'modal-transform')
+    }, 500)
   }
 
-
-
-
-
-
-
-
-
-
 })
-
