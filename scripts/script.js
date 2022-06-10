fa980497586e2736f9f3377b82793f502a89e1ac
Konst_V <varjutin@yandex.ru>
2022-06-10 23:40:17 +0300

    Add functions in js file

--- scripts/script.js
+++ scripts/script.js
@@ -105,32 +105,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const btnMoreList = document.querySelectorAll(".btn-more");
   const heroInfo = document.querySelector(".hero-info");
+  const btnMore  = document.querySelector(".btn-more--hero")
 
-  document
-    .querySelector(".btn-more--hero")
-    .addEventListener("click", function () {
-      if (heroInfo.clientHeight < heroInfo.scrollHeight) {
-        heroInfo.style.maxHeight =
-          document.querySelector(".hero-info").scrollHeight + "px";
-        this.innerHTML =
-          '<img class="btn-more__icon" src="./assets/images/expand-up.svg" alt="Двойная стрелка вверх">Скрыть';
-        if (window.innerWidth > 767) {
-          showMaxHeight(heroInfo,400)
-        } else {
-          showMaxHeight(heroInfo,500)
-        }
-      } else {
-        this.innerHTML =
-          '<img class="btn-more__icon" src="./assets/images/expand.svg" alt="Двойная стрелка вниз">Читать далее';
-        if (window.innerWidth >= 1120) {
-          hideMaxHeight(heroInfo,165,100)
-        } else if (window.innerWidth < 768) {
-          hideMaxHeight(heroInfo,90,10)
-        } else {
-          hideMaxHeight(heroInfo,160,250)
-        }
+
+  document.querySelector(".btn-more--hero")
+    .addEventListener("click", function() {
+        let func = elementHeightControl.bind(this,heroInfo)
+        func();
       }
-    });
+    );
+
 
   let i;
 
@@ -148,8 +132,8 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
 
-    // }
   }
+
 
   const btnBurger = document.querySelector(".burger");
   const mobileMenu = document.querySelector(".mobile-menu");
@@ -220,4 +204,30 @@ document.addEventListener("DOMContentLoaded", () => {
       elem.style.overflow = "visible";
     }, delay);
   }
+
+  function elementHeightControl(element) {
+    if (element.clientHeight < element.scrollHeight) {
+      element.style.maxHeight =
+        element.scrollHeight + "px";
+      this.innerHTML =
+        '<img class="btn-more__icon" src="./assets/images/expand-up.svg" alt="Двойная стрелка вверх">Скрыть';
+      if (window.innerWidth > 767) {
+        showMaxHeight(element,400)
+      } else {
+        showMaxHeight(element,500)
+      }
+    } else {
+      this.innerHTML =
+        '<img class="btn-more__icon" src="./assets/images/expand.svg" alt="Двойная стрелка вниз">Читать далее';
+      if (window.innerWidth >= 1120) {
+        hideMaxHeight(element,165,100)
+      } else if (window.innerWidth < 768) {
+        hideMaxHeight(element,90,10)
+      } else {
+        hideMaxHeight(element,160,250)
+      }
+    }
+  }
+
+
 });
